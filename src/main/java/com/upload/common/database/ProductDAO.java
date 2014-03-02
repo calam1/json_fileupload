aca1f5e1a7e675cd889116a799ee8f0f070b837e
Christopher Lam <chrislam67@gmail.com>
2014-03-02 15:22:39 -0600

    addded executor service in spring to simulate db insert

---
+++ src/main/java/com/upload/common/database/ProductDAO.java
@@ -0,0 +1,14 @@
+package com.upload.common.database;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: christopherlam
+ * Date: 3/2/14
+ * Time: 11:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProductDAO {
+    public String add(String product){
+        return product;
+    }
+}
--- src/main/java/com/upload/common/messaging/Listener.java
+++ src/main/java/com/upload/common/messaging/Listener.java
@@ -1,9 +1,14 @@
 package com.upload.common.messaging;
 
+import com.upload.common.database.ProductDAO;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -15,16 +20,48 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class Listener implements MessageListener {
 
-  public static final AtomicLong counter = new AtomicLong(0L);
+    public static final AtomicLong counter = new AtomicLong(0L);//just for manual testing
+    private ProductDAO productDAO;
+    private ExecutorService executorService;
 
-  @Override
-  public void onMessage(Message message) {
-    if (message instanceof TextMessage) {
-      long count = counter.incrementAndGet(); 
-      System.out.println( "LISTENER - COUNTER: " + count);
+    public ExecutorService getExecutorService() {
+        return executorService;
     }
-    else {
-      throw new IllegalArgumentException("Message must be of type TextMessage");
+
+    public void setExecutorService(ExecutorService executorService) {
+        this.executorService = executorService;
     }
-  }
+
+    public ProductDAO getProductDAO() {
+        return productDAO;
+    }
+
+    public void setProductDAO(ProductDAO productDAO) {
+        this.productDAO = productDAO;
+    }
+
+    @Override
+    public void onMessage(final Message message) {
+        if (message instanceof TextMessage) {
+
+            Future<String> future = executorService.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return productDAO.add(((TextMessage) message).getText());
+                }
+            });
+
+            try {
+                long count = counter.incrementAndGet();
+                System.out.println("Count is: " + count + " Future.get() returns: " + future.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            throw new IllegalArgumentException("Message must be of type TextMessage");
+        }
+    }
 }
